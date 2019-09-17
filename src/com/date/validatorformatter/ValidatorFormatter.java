3ad7b151d923099167a57b165977e8a2946c8ca0
Amey Hinge <ameyhinge@outlook.com>
2019-09-17 09:34:59 +0530

    Added: Day Maps Updated: Validator

--- src/com/date/validatorformatter/ValidatorFormatter.java
+++ src/com/date/validatorformatter/ValidatorFormatter.java
@@ -43,7 +43,45 @@ public class ValidatorFormatter implements ValidateFormat {
 		monthMap.put(10, "October");
 		monthMap.put(11, "November");
 		monthMap.put(12, "December");
+
+		Map dayMap = new HashMap<Integer, String>();
+		dayMap.put(1, "Monday");
+		dayMap.put(2, "Tuesday");
+		dayMap.put(3, "Wednesday");
+		dayMap.put(4, "Thursday");
+		dayMap.put(5, "Friday");
+		dayMap.put(6, "Saturday");
+		dayMap.put(7, "Sunday");
+
+		String[] monthFormats = { "mm", "mmm", "mmmm" };
+		String[] yearFormats = { "yy", "yyyy" };
+		String[] dayFormats = { "dd", "ddd", "dddd" };
+
+		StringBuilder inputFirstPart = null;
+		StringBuilder inputSecondPart = null;
+		StringBuilder inputThirdPart = null;
+		StringBuilder inputDelimiter = null;
+		inputDateFormat = inputDateFormat.toUpperCase();
+		System.out.println(inputDateFormat);
+		try {
+			if (inputDateFormat.length() != 10 && inputDateFormat.length() != 8 && inputDateFormat.length() != 6
+					&& inputDateFormat.length() != 12) {
+				throw new Exception("ERROR : Invalid value for 'Date Format'. ");
+			} else {
+				// Check if input Format is delimited
+				char delimiter;
+				for (int i = 0; i < inputDateFormat.length(); i++) {
+					if (inputDateFormat.charAt(i) != 'D' && inputDateFormat.charAt(i) != 'M'
+							&& inputDateFormat.charAt(i) != 'Y') {
+						delimiter = inputDateFormat.charAt(i);
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
-
 }
